cd31cf7388c83e0fad4e4b12eee8a405d107a981
Rafael Velazquez Carmona <rafavela01@Rafaels-MacBook-Air.local>
2024-09-11 18:57:57 -0600

    Add a profile successfully and fetch it successfully

--- tinder-ai-backend/src/main/java/io/javabrains/tinder_ai_backend/TinderAiBackendApplication.java
+++ tinder-ai-backend/src/main/java/io/javabrains/tinder_ai_backend/TinderAiBackendApplication.java
@@ -1,13 +1,31 @@
 package io.javabrains.tinder_ai_backend;
 
+import io.javabrains.tinder_ai_backend.profiles.Gender;
+import io.javabrains.tinder_ai_backend.profiles.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.javabrains.tinder_ai_backend.profiles.Profile;
 
 @SpringBootApplication
-public class TinderAiBackendApplication {
+public class TinderAiBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
 	}
 
+	public void run(String... args){
+		System.out.println("My app is running");
+		Profile profile = new Profile(
+				"1","Rafael","Velazquez",40, "Latinx", Gender.MALE,"Software programmer","foo.jpg","INTP"
+		);
+		profileRepository.save(profile);
+		profileRepository.findAll().forEach(System.out::println);
+	}
 }
+
+
---
+++ tinder-ai-backend/src/main/java/io/javabrains/tinder_ai_backend/profiles/Gender.java
@@ -0,0 +1,5 @@
+package io.javabrains.tinder_ai_backend.profiles;
+
+public enum Gender {
+    MALE,FEMALE,NON_BINARY
+}
