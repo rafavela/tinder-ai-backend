117a40fff32c748686a484092c6735192aea2caa
Rafael Velazquez Carmona <rafavela01@Rafaels-MacBook-Air.local>
2024-09-11 19:17:44 -0600

    Add conversation Model and repository

--- tinder-ai-backend/src/main/java/io/javabrains/tinder_ai_backend/TinderAiBackendApplication.java
+++ tinder-ai-backend/src/main/java/io/javabrains/tinder_ai_backend/TinderAiBackendApplication.java
@@ -1,18 +1,27 @@
 package io.javabrains.tinder_ai_backend;
 
+import io.javabrains.tinder_ai_backend.conversations.ChatMessage;
+import io.javabrains.tinder_ai_backend.conversations.Conversation;
+import io.javabrains.tinder_ai_backend.conversations.ConversationRepository;
 import io.javabrains.tinder_ai_backend.profiles.Gender;
+import io.javabrains.tinder_ai_backend.profiles.Profile;
 import io.javabrains.tinder_ai_backend.profiles.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.javabrains.tinder_ai_backend.profiles.Profile;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class TinderAiBackendApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProfileRepository profileRepository;
+
+	@Autowired
+	private ConversationRepository conversationRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
@@ -25,6 +34,17 @@ public class TinderAiBackendApplication implements CommandLineRunner {
 		);
 		profileRepository.save(profile);
 		profileRepository.findAll().forEach(System.out::println);
+
+		Conversation conversation = new Conversation(
+				"1",
+				profile.id(),
+				List.of(
+					new ChatMessage("hello", profile.id(), LocalDateTime.now())
+				)
+		);
+
+		conversationRepository.save(conversation);
+		conversationRepository.findAll().forEach(System.out::println);
 	}
 }
 
