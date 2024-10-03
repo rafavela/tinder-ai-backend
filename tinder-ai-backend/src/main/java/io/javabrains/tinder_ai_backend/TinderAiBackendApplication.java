3ff5768b4b1ad99859ab5b0b975ad6e110cdbc32
Rafael Velazquez Carmona <rafavela01@Rafaels-MacBook-Air.local>
2024-10-03 12:09:20 -0600

    Profile creation

--- tinder-ai-backend/src/main/java/io/javabrains/tinder_ai_backend/TinderAiBackendApplication.java
+++ tinder-ai-backend/src/main/java/io/javabrains/tinder_ai_backend/TinderAiBackendApplication.java
@@ -1,60 +1,23 @@
 package io.javabrains.tinder_ai_backend;
 
-import io.javabrains.tinder_ai_backend.conversations.ChatMessage;
-import io.javabrains.tinder_ai_backend.conversations.Conversation;
-import io.javabrains.tinder_ai_backend.conversations.ConversationRepository;
-import io.javabrains.tinder_ai_backend.profiles.Gender;
-import io.javabrains.tinder_ai_backend.profiles.Profile;
-import io.javabrains.tinder_ai_backend.profiles.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @SpringBootApplication
 public class TinderAiBackendApplication implements CommandLineRunner {
 
 	@Autowired
-	private ProfileRepository profileRepository;
-
-	@Autowired
-	private ConversationRepository conversationRepository;
+	private io.javabrains.tinder_ai_backend.profiles.ProfileCreationService profileCreationService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
 	}
 
 	public void run(String... args){
-		System.out.println("My app is running");
-
-		profileRepository.deleteAll();
-		conversationRepository.deleteAll();
-
-		Profile profile = new Profile(
-				"1","Rafael","Velazquez",40, "Latinx", Gender.MALE,"Software programmer","foo.jpg","INTP"
-		);
-
-		profileRepository.save(profile);
-
-		profile = new Profile(
-				"2","Foo","Bar",40, "Latinx", Gender.MALE,"Software programmer","foo.jpg","INTP"
-		);
-		profileRepository.save(profile);
-		profileRepository.findAll().forEach(System.out::println);
-
-		Conversation conversation = new Conversation(
-				"1",
-				profile.id(),
-				List.of(
-					new ChatMessage("hello", profile.id(), LocalDateTime.now())
-				)
-		);
-
-		conversationRepository.save(conversation);
-		conversationRepository.findAll().forEach(System.out::println);
+		profileCreationService.createProfiles(0);
+		profileCreationService.saveProfilesToDB();
 	}
 }
 
